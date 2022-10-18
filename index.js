b0515dd49115ca1c7fbcb58068f2c4e72dc3730f
Dmitriy <dmitrvad9@gmail.com>
2022-10-18 22:53:40 +0300

    web token

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+.connect('mongodb+srv://minda:minda334@cluster0.9gkpakf.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('DB ok'))
+.catch((err) => console.log('DB error', err));
 
 const app = express();
 
@@ -10,8 +17,17 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
     console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Вася Пупкин',
+    },
+        'secret123',
+    );
+
     res.json({
         success: true,
+        token,
     });
 });
 
